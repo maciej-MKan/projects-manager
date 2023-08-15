5c198b47bf479bc7a01471d196136784a72a99db
MKan <kanabaj.maciej@gmail.com>
2023-08-15 14:48:34 +0200

    move pyramid api to external repository

--- backend/src/infrastructure/securite/authenticate.py
+++
@@ -1,9 +0,0 @@
-from backend.src.infrastructure.configuration.application_configuration import ApplicationConfiguration
-
-user_service = ApplicationConfiguration().get_user_service()
-
-
-def authenticate(login, password):
-    login_user = user_service.login_user(login, password)
-
-    return login_user
